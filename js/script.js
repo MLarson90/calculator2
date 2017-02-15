270883b666585db0725342b8801c5140a2541418
Max Larson <mr.m.larson90@gmail.com>
2017-02-15 11:21:57 -0800

    fully functioning user input addition calculator

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@
   //front-end logic:
   $(document).ready(function(){
     $('form#add').submit(function(event){
+      event.preventDefault();
     var number1 = parseInt($('#add1').val());
     var number2 = parseInt($('#add2').val());
-    alert(add(number1,number2));
+    var result = addition(number1,number2);
+    $('.output').text(result);
   });
 });
