84b241a18bbccbae6f841536b57cd5ace8b47092
Max Larson <mr.m.larson90@gmail.com>
2017-02-15 11:34:27 -0800

    add subtraction multiplication and divition to calculator

--- js/script.js
+++ js/script.js
@@ -3,13 +3,43 @@
   var addition = function(number1,number2){
     return number1 + number2;
   };
+  var subtract = function(number1,number2){
+    return number1 - number2;
+  };
+  var multiply = function(number1,number2){
+    return number1 * number2;
+  };
+  var divid = function(number1,number2){
+    return number1 / number2;
+  };
   //front-end logic:
   $(document).ready(function(){
-    $('form#add').submit(function(event){
+    $('form#sub').submit(function(event){
       event.preventDefault();
-    var number1 = parseInt($('#add1').val());
-    var number2 = parseInt($('#add2').val());
-    var result = addition(number1,number2);
+    var number1 = parseInt($('#sub1').val());
+    var number2 = parseInt($('#sub2').val());
+    var result = subtract(number1,number2);
     $('.output').text(result);
   });
+  $('form#mult').submit(function(event){
+    event.preventDefault();
+  var number1 = parseInt($('#mult1').val());
+  var number2 = parseInt($('#mult2').val());
+  var result = multiply(number1,number2);
+  $('.output').text(result);
+});
+$('form#add').submit(function(event){
+  event.preventDefault();
+var number1 = parseInt($('#add1').val());
+var number2 = parseInt($('#add2').val());
+var result = addition(number1,number2);
+$('.output').text(result);
+});
+$('form#div').submit(function(event){
+  event.preventDefault();
+var number1 = parseInt($('#div1').val());
+var number2 = parseInt($('#div2').val());
+var result = divid(number1,number2);
+$('.output').text(result);
+});
 });
